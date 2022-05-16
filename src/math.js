661a65837a5134abc89e12c4496696183d2fd804
Liam Bell <Liam.Bell@togethermoney.com>
2022-05-16 13:01:22 +0100

    nCr always returns integer

--- src/math.js
+++ src/math.js
@@ -1,12 +1,13 @@
 const factorial = (n) => {
-  if (n < 0) throw "Cannot calculate factorials of negative numbers"
+  if (!Number.isInteger(n)) throw "Cannot calculate factorials for decimal numbers"
+  if (n < 0) throw "Cannot calculate factorials for negative numbers"
   return n <= 1 ? 1 : n * factorial(n - 1)
 }
 
-const ncr = (n, r) => {
+const nCr = (n, r) => {
   if (r < 0) throw "r cannot be less than zero"
   if (r > n) throw "r cannot be greater than n"
-  return factorial(n) / (factorial(r) * factorial(n - r))
+  return parseInt(factorial(n) / (factorial(r) * factorial(n - r)))
 }
 
-export { ncr }
+export { nCr }
